4a61ec6a16fcd6f2195b48e3f5f512f814fad9e7
Balázs <info@balazsorban.com>
2017-09-25 13:46:17 +0200

    :art: :sparkles: Add Bookings Overview functionality.

--- src/components/BookingBoss/index.js
+++ src/components/BookingBoss/index.js
@@ -1,54 +1,117 @@
 import React, { Component } from 'react'
-import AppBar from 'material-ui/AppBar'
+import firebase from 'firebase'
 import Drawer from 'material-ui/Drawer'
-import Avatar from 'material-ui/Avatar'
 import MenuItem from 'material-ui/MenuItem'
-import RaisedButton from 'material-ui/RaisedButton'
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import {green100, green500, green700} from 'material-ui/styles/colors'
+import {Tabs, Tab} from 'material-ui/Tabs'
 
 
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: green500,
-    primary2Color: green700,
-    primary3Color: green100,
-    accent1Color: green700
-  },
-})
+const parseDate = date => new Date(date).toISOString().slice(0, 10)
 
-
-
-const profiles = require.context('../../img/profiles');
 
 export default class BookingBoss extends Component {
-
-  render() {
-    const {user, isDrawerOpened, toggleDrawer, logout} = this.props
-    const {uid, img, name} = user
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div>
-          <Drawer open={isDrawerOpened}>
-            <div className="user-info">
-              <Avatar src={profiles(`./${img}.jpg`)}/>
-              <h2>{name}</h2>
-            </div>
-            <MenuItem primaryText="Menu 1" />
-            <RaisedButton onClick={() => logout()} label="Logout"/>
-          </Drawer>
-          <AppBar   onLeftIconButtonTouchTap={() => toggleDrawer()}  title="Event Manager"
-          />
-
-          <div>
-            <h3>
-              Booking Boss {uid}
-            </h3>
-          </div>
-        </div>
-    </MuiThemeProvider>
-    );
+  constructor() {
+    super()
+    this.state = {
+      concerts: {},
+      bands: {}
+    }
   }
+
+  handleBooking(concert, isAccepted) {
+    const db = firebase.database().ref()
+    db.child(`concerts/${concert}/isAcceptedByBookingBoss`).set(isAccepted)
+  }
+  componentDidMount() {
+    const db = firebase.database().ref()
+    const concertsRef = db.child('concerts')
+    const bandsRef = db.child('bands')
+    concertsRef.on('value', snap => {
+      this.setState({
+        concerts: snap.val()
+      })
+    })
+    bandsRef.on('value', snap => {
+      this.setState({
+        bands: snap.val()
+      })
+    })
+  }
+  render() {
+    const {isDrawerOpened, toggleDrawer} = this.props
+    const {bands, concerts} = this.state
+    const unhandledBookings = []
+    let unhandledCounter = 0
+    const acceptedBookings = []
+    let acceptedCounter = 0
+    const rejectedBookings = []
+    let rejectedCounter = 0
+
+    Object.keys(concerts).forEach(key => {
+      const concert = concerts[key]
+      const {band, ticketPrice, from, to, isAcceptedByBookingManager, isAcceptedByBookingBoss} = concert
+      const bandName = bands[band] && bands[band].name
+      if (isAcceptedByBookingManager) {
+        if (isAcceptedByBookingBoss==="unhandled") {
+          unhandledBookings.push(
+            <li key={band}>
+              <p>{bandName}</p>
+              <p>{ticketPrice} NOK</p>
+              <p>{parseDate(from)}</p>
+              <p>{parseDate(to)}</p>
+              <button onClick={(band, isAccepted) => this.handleBooking(key, true)}>Accept</button>
+              <button onClick={(band, isAccepted) => this.handleBooking(key, false)}>Reject</button>
+            </li>)
+            unhandledCounter+=1
+        } else if (isAcceptedByBookingBoss) {
+          acceptedBookings.push(
+            <li key={band}>
+              <p>Accepted</p>
+              <p>{bandName}</p>
+              <p>{ticketPrice} NOK</p>
+              <p>{parseDate(from)}</p>
+              <p>{parseDate(to)}</p>
+            </li>)
+            acceptedCounter+=1
+        } else {
+          rejectedBookings.push(
+            <li key={band}>
+              <p>Rejected</p>
+              <p>{bandName}</p>
+              <p>{ticketPrice} NOK</p>
+              <p>{parseDate(from)}</p>
+              <p>{parseDate(to)}</p>
+            </li>)
+          rejectedCounter+=1
+        }
+      }
+    })
+    return (
+        <div>
+          <Drawer
+            open={isDrawerOpened}>
+            <MenuItem onClick={() => toggleDrawer()} primaryText="Bookings Overview" />
+          </Drawer>
+          <Tabs>
+            <Tab label={`New bookings (${unhandledCounter})`}>
+              <Bookings bookings={unhandledBookings}/>
+            </Tab>
+            <Tab label={`Accepted bookings (${acceptedCounter})`}>
+              <Bookings bookings={acceptedBookings}/>
+            </Tab>
+            <Tab label={`Rejected bookings (${rejectedCounter})`}>
+              <Bookings bookings={rejectedBookings} />
+            </Tab>
+          </Tabs>
+        </div>
+    )
+  }
+}
+
+
+const Bookings = ({bookings}) => {
+  return (
+    <ul>
+      {bookings}
+    </ul>
+  )
 }
