2584c793c193c1d77d0f38c2f057425e7786aa81
Balázs <info@balazsorban.com>
2017-09-25 14:58:48 +0200

    :art: Add Material UI CSS framework.

--- src/components/BookingBoss/index.js
+++ src/components/BookingBoss/index.js
@@ -45,42 +45,67 @@ export default class BookingBoss extends Component {
     let acceptedCounter = 0
     const rejectedBookings = []
     let rejectedCounter = 0
-
+    console.log(concerts);
+    console.log(bands);
     Object.keys(concerts).forEach(key => {
       const concert = concerts[key]
-      const {band, ticketPrice, from, to, isAcceptedByBookingManager, isAcceptedByBookingBoss} = concert
+      const {band, ticketPrice, from, isAcceptedByBookingManager, isAcceptedByBookingBoss} = concert
       const bandName = bands[band] && bands[band].name
       if (isAcceptedByBookingManager) {
         if (isAcceptedByBookingBoss==="unhandled") {
           unhandledBookings.push(
             <li key={band}>
-              <p>{bandName}</p>
-              <p>{ticketPrice} NOK</p>
-              <p>{parseDate(from)}</p>
-              <p>{parseDate(to)}</p>
-              <button onClick={(band, isAccepted) => this.handleBooking(key, true)}>Accept</button>
-              <button onClick={(band, isAccepted) => this.handleBooking(key, false)}>Reject</button>
+              <div className=" mdl-card mdl-shadow--2dp">
+                <div className="mdl-card__title mdl-card--expand">
+                  <h4>
+                    {bandName}<br/>
+                    {parseDate(from)}<br/>
+                    {ticketPrice} NOK<br/>
+                  </h4>
+                </div>
+                <div className="booking-footer mdl-card__actions mdl-card--border">
+                  <a onClick={(id, isAccepted) => this.handleBooking(key, true)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Accept</a>
+                  <a onClick={(id, isAccepted) => this.handleBooking(key, false)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Reject</a>                <div className="mdl-layout-spacer"></div>
+                  <i className="material-icons">event</i>
+                </div>
+              </div>
             </li>)
-            unhandledCounter+=1
-        } else if (isAcceptedByBookingBoss) {
-          acceptedBookings.push(
-            <li key={band}>
-              <p>Accepted</p>
-              <p>{bandName}</p>
-              <p>{ticketPrice} NOK</p>
-              <p>{parseDate(from)}</p>
-              <p>{parseDate(to)}</p>
-            </li>)
-            acceptedCounter+=1
-        } else {
-          rejectedBookings.push(
-            <li key={band}>
-              <p>Rejected</p>
-              <p>{bandName}</p>
-              <p>{ticketPrice} NOK</p>
-              <p>{parseDate(from)}</p>
-              <p>{parseDate(to)}</p>
-            </li>)
+              unhandledCounter+=1
+              } else if (isAcceptedByBookingBoss) {
+                acceptedBookings.push(
+                  <li key={band}>
+                    <div className=" mdl-card mdl-shadow--2dp">
+                      <div className="mdl-card__title mdl-card--expand">
+                        <h4>
+                          {bandName}<br/>
+                          {parseDate(from)}<br/>
+                          {ticketPrice} NOK<br/>
+                        </h4>
+                      </div>
+                      <div className="booking-footer mdl-card__actions mdl-card--border">
+                        Accepted<div className="mdl-layout-spacer"></div>
+                        <i className="material-icons">done</i>
+                      </div>
+                    </div>
+                  </li>)
+                acceptedCounter+=1
+              } else {
+                rejectedBookings.push(
+                  <li key={band}>
+                    <div className=" mdl-card mdl-shadow--2dp">
+                      <div className="mdl-card__title mdl-card--expand">
+                        <h4>
+                          {bandName}<br/>
+                          {parseDate(from)}<br/>
+                          {ticketPrice} NOK<br/>
+                        </h4>
+                      </div>
+                      <div className="booking-footer mdl-card__actions mdl-card--border">
+                        Rejected<div className="mdl-layout-spacer"></div>
+                        <i className="material-icons">clear</i>
+                      </div>
+                    </div>
+                  </li>)
           rejectedCounter+=1
         }
       }
@@ -110,7 +135,7 @@ export default class BookingBoss extends Component {
 
 const Bookings = ({bookings}) => {
   return (
-    <ul>
+    <ul className="booking-list">
       {bookings}
     </ul>
   )
