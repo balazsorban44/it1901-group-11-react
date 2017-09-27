0c6b446320b49548b371bd0a98d1b0b7e7e0a2db
supertosse <supertosse@hotmail.com>
2017-09-27 14:32:11 +0200

    Add items in menu

--- src/components/BookingManager/index.js
+++ src/components/BookingManager/index.js
@@ -1,54 +1,88 @@
 import React, { Component } from 'react'
-import AppBar from 'material-ui/AppBar'
+import firebase from 'firebase'
 import Drawer from 'material-ui/Drawer'
-import Avatar from 'material-ui/Avatar'
 import MenuItem from 'material-ui/MenuItem'
-import RaisedButton from 'material-ui/RaisedButton'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import {green100, green500, green700} from 'material-ui/styles/colors'
-
-
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: green500,
-    primary2Color: green700,
-    primary3Color: green100,
-    accent1Color: green700
-  },
-})
-
-
-
-const profiles = require.context('../../img/profiles');
 
 export default class BookingManager extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // initializing local concerts
+      openedMenuItem: ""
+    }
+  }
+
+// Fetching content from firebase
+componentDidMount(){
+  // referencing database (firebase) "ready up for connect"
+  const db = firebase.database().ref()
+  // accesing child of database = concerts
+
+}
+
+handleMenuItemClick(openedMenuItem){
+  this.props.toggleDrawer()
+  this.setState({openedMenuItem})
+}
 
   render() {
-    const {user, isDrawerOpened, toggleDrawer, logout} = this.props
-    const {uid, img, name} = user
+    const {isDrawerOpened} = this.props
+    const {openedMenuItem} = this.state
+
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <Drawer open={isDrawerOpened}>
-            <div className="user-info">
-              <Avatar src={profiles(`./${img}.jpg`)}/>
-              <h2>{name}</h2>
-            </div>
-            <MenuItem primaryText="Menu 1" />
-            <RaisedButton onClick={() => logout()} label="Logout"/>
+            <MenuItem onClick={() => this.handleMenuItemClick("component")} primaryText="Booking Manager" />
+            <MenuItem onClick={() => this.handleMenuItemClick("MakeOffer")} primaryText="Make offer" />
+            <MenuItem onClick={() => this.handleMenuItemClick("PreviousConserts")} primaryText="Previous conserts" />
+            <MenuItem onClick={() => this.handleMenuItemClick("Search")} primaryText="Search" />
           </Drawer>
-          <AppBar   onLeftIconButtonTouchTap={() => toggleDrawer()}  title="Event Manager"
-          />
 
-          <div>
-            <h3>
-              Booking Manager {uid}
-            </h3>
-          </div>
+          <make_offer name = "hei"/>
+          <MakeOffer/>
+          {{
+            "MakeOffer":
+            <MakeOffer/>,
+            "PreviousConserts":
+            <PreviousConserts/>,
+            "Search":
+            <Search/>
+
+          }[openedMenuItem]}
+
         </div>
-    </MuiThemeProvider>
-    );
-  }
+    )}
 }
+
+const MakeOffer = () =>
+      <div>
+        <h1> Make offer</h1>
+
+        <form action="#">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1"/>
+            <label class="mdl-textfield__label" for="sample1">Text...</label>
+          </div>
+        </form>
+        <form action="#">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="sample1"/>
+            <label class="mdl-textfield__label" for="sample1">Text...</label>
+          </div>
+        </form>
+
+        <form>
+          <h5>Band name</h5>
+          <input type="text" name="firstname"/>
+          <h5>Price</h5>
+          <input type="text" name="lastname"/>
+        </form>
+      </div>
+
+
+const Search = () =>
+      <h1>Search</h1>
+
+const PreviousConserts = () =>
+      <h1>Previous consertsr</h1>
