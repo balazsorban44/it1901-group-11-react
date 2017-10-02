601930d2f01c6923c5621a05503e9540fa319893
Balázs <info@balazsorban.com>
2017-10-02 19:35:31 +0200

    :art: Restructure component for less recursive code, and show more info.
    Created Booking component, and added the Event's name to a booking, for
    better overview. Also changed the Accept/Reject buttons to RaisedButton
    from simple <a> tags.

--- src/components/BookingBoss/index.js
+++ src/components/BookingBoss/index.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import firebase from 'firebase'
 import Drawer from 'material-ui/Drawer'
 import Badge from 'material-ui/Badge'
+import RaisedButton from 'material-ui/RaisedButton'
 import MenuItem from 'material-ui/MenuItem'
 import BookingTabSwipe from './BookingTabSwipe'
-import {parseDate} from '../../utils'
+import {parseDate, parseTime} from '../../utils'
 
 
 export default class BookingBoss extends Component {
@@ -16,19 +17,45 @@ export default class BookingBoss extends Component {
     }
   }
 
-  handleBooking(concert, isAccepted) {
-    const db = firebase.database().ref()
-    db.child(`concerts/${concert}/isAcceptedByBookingBoss`).set(isAccepted)
-  }
+
   componentDidMount() {
     const db = firebase.database().ref()
     const concertsRef = db.child('concerts')
     const bandsRef = db.child('bands')
-    concertsRef.on('value', snap => {
-      this.setState({
-        concerts: snap.val()
+    const eventsRef = db.child('events')
+    const scenesRef = db.child('scenes')
+
+    eventsRef.on('value', snap => {
+      const events = snap.val()
+      Object.keys(events).forEach(eventKey => {
+        const event = events[eventKey]
+        const {location, name} = event
+        if (event.staff.bookingBoss.includes(this.props.user.uid)) {
+          const {scenes} = event
+          scenes.forEach(sceneKey => {
+            scenesRef.child(sceneKey).on('value', snap => {
+              const {concerts} = snap.val()
+              concerts.forEach(concertKey => {
+                concertsRef.child(concertKey).on('value', snap => {
+                  const concert = snap.val()
+                  concert.eventName = name
+
+                  this.setState(prevState => ({
+                    concerts: {
+                      ...prevState.concerts,
+                      [concertKey]: concert
+                    }
+                  }))
+                })
+              })
+            })
+          })
+        } else {
+          delete events[eventKey]
+        }
       })
     })
+
     bandsRef.on('value', snap => {
       this.setState({
         bands: snap.val()
@@ -47,92 +74,92 @@ export default class BookingBoss extends Component {
     let rejectedCounter = 0
     Object.keys(concerts).forEach(key => {
       const concert = concerts[key]
-      const {band, ticketPrice, from, isAcceptedByBookingManager, isAcceptedByBookingBoss} = concert
+      const {eventName, band, ticketPrice, from, isAcceptedByBookingManager, isAcceptedByBookingBoss} = concert
       const bandName = bands[band] && bands[band].name
+      const booking = <Booking
+        key={key}
+        {...{eventName, bandName, from, ticketPrice}}
+        concertKey={key}
+        bookingState={isAcceptedByBookingBoss}
+                      />
       if (isAcceptedByBookingManager) {
         if (isAcceptedByBookingBoss==="unhandled") {
-          unhandledBookings.push(
-            <li key={band}>
-              <div className="mdl-card mdl-shadow--2dp">
-                <div className="mdl-card__title mdl-card--expand">
-                  <span className="booking-bg"/>
-                  <h4 className="booking-text">
-                    {bandName}<br/>
-                    {parseDate(from)}<br/>
-                    {ticketPrice} NOK<br/>
-                  </h4>
-                </div>
-                <div className="booking-footer mdl-card__actions mdl-card--border">
-                  <a onClick={(id, isAccepted) => this.handleBooking(key, true)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Accept</a>
-                  <a onClick={(id, isAccepted) => this.handleBooking(key, false)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Reject</a>                <div className="mdl-layout-spacer"></div>
-                  <i className="material-icons">event</i>
-                </div>
-              </div>
-            </li>)
-              unhandledCounter+=1
-              } else if (isAcceptedByBookingBoss) {
-                acceptedBookings.push(
-                  <li key={band}>
-                    <div className="mdl-card mdl-shadow--2dp">
-                      <span className="booking-bg"/>
-                      <div className="mdl-card__title mdl-card--expand">
-                        <h4 className="booking-text">
-                          {bandName}<br/>
-                        </h4>
-                        <h5>
-                          {parseDate(from)}<br/>
-                          {ticketPrice} NOK<br/>
-                        </h5>
-                      </div>
-                      <div className="booking-footer mdl-card__actions mdl-card--border">
-                        Accepted<div className="mdl-layout-spacer"></div>
-                        <i className="material-icons green">done</i>
-                      </div>
-                    </div>
-                  </li>)
-                acceptedCounter+=1
-              } else {
-                rejectedBookings.push(
-                  <li key={band}>
-                    <div className="mdl-card mdl-shadow--2dp">
-                      <div className="mdl-card__title mdl-card--expand">
-                        <span className="booking-bg"/>
-                        <h4 className="booking-text">
-                          {bandName}<br/>
-                          {parseDate(from)}<br/>
-                          {ticketPrice} NOK<br/>
-                        </h4>
-                      </div>
-                      <div className="booking-footer mdl-card__actions mdl-card--border">
-                        Rejected<div className="mdl-layout-spacer"></div>
-                        <i className="material-icons red">clear</i>
-                      </div>
-                    </div>
-                  </li>)
+          unhandledBookings.push(booking)
+          unhandledCounter+=1
+        } else if (isAcceptedByBookingBoss) {
+          acceptedBookings.push(booking)
+          acceptedCounter+=1
+        } else {
+          rejectedBookings.push(booking)
           rejectedCounter+=1
         }
       }
     })
     return (
-        <div id="booking-boss">
-          <Drawer
-            docked={false}
-            open={isDrawerOpened}>
-            <MenuItem onClick={() => toggleDrawer()} primaryText={
-              <div>
-                Bookings Overview
-                <Badge
-                  badgeContent={unhandledCounter}
-                  secondary={true}
-                  badgeStyle={{top: 18, right: 6}}
-                />
-              </div>
-            }>
+      <div id="booking-boss">
+        <Drawer
+          docked={false}
+          open={isDrawerOpened}>
+          <MenuItem onClick={() => toggleDrawer()} primaryText={
+            <div>
+              Bookings Overview
+              <Badge
+                badgeContent={unhandledCounter}
+                secondary={true}
+                badgeStyle={{top: 18, right: 6}}
+              />
+            </div>
+          }>
 
-            </MenuItem>
-          </Drawer>
-          <BookingTabSwipe {...{unhandledCounter, unhandledBookings, acceptedCounter, acceptedBookings, rejectedCounter, rejectedBookings}}/>
-        </div>
+          </MenuItem>
+        </Drawer>
+        <BookingTabSwipe {...{unhandledCounter, unhandledBookings, acceptedCounter, acceptedBookings, rejectedCounter, rejectedBookings}}/>
+      </div>
     )
   }
+}
+
+
+const Booking = ({eventName, bandName, from, ticketPrice, bookingState, concertKey}) => {
+
+  const handleBooking = (concert, isAccepted) => {
+    const db = firebase.database().ref()
+    db.child(`concerts/${concert}/isAcceptedByBookingBoss`).set(isAccepted)
+  }
+
+  let bookingStateAction = <div>
+    <RaisedButton className="accept-button" label="Accept" primary onClick={(id, isAccepted) => handleBooking(concertKey, true)}/>
+    <RaisedButton label="Reject" secondary onClick={(id, isAccepted) => handleBooking(concertKey, false)}/>
+  </div>
+  let icon = "bookmark_border"
+  let color = ""
+  if (bookingState === true) {
+    bookingStateAction = "Accepted"
+    icon = "done"
+    color = "green"
+  } else if (bookingState === false) {
+    bookingStateAction = "Rejected"
+    icon = "clear"
+    color = "red"
+  }
+  return (
+    <li>
+      <div className="mdl-card mdl-shadow--2dp">
+        <div className="booking-body mdl-card__title mdl-card--expand">
+          <span className="booking-bg"/>
+          <h4>{bandName} <br/>@<br/></h4>
+          <h5>
+            {eventName} <br/>
+            {parseDate(from)} - {parseTime(from)}
+            <br/>
+            {ticketPrice} NOK
+          </h5>
+        </div>
+        <div className="booking-footer mdl-card__actions mdl-card--border">
+          {bookingStateAction}<div className="mdl-layout-spacer"></div>
+          <i className={`material-icons ${color}`}>{icon}</i>
+        </div>
+      </div>
+    </li>
+  )
 }
