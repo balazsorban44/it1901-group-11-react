445445eb94f60072b722f7af25aea6ed4abe9f75
matsjp <smuckenduck@gmail.com>
2017-10-19 13:25:48 +0200

    Fix member names

--- src/components/Manager/index.js
+++ src/components/Manager/index.js
@@ -22,14 +22,28 @@ export default class Manager extends Component {
   componentDidMount(){
     const db = firebase.database().ref()
     const bandsRef = db.child('bands')
+    const staffRef = db.child('staff')
     bandsRef.on('value', snap => {
-      const bands = snap.val()
+      let bands = snap.val()
       Object.keys(bands).forEach(bandKey => {
         if (bands[bandKey]['manager'] !== this.props.user['uid']){
           delete(bands[bandKey])
         }
+        else{
+          let memberNames = []
+          const members = bands[bandKey].members
+          staffRef.child('profiles').on('value', snap => {
+            const profiles = snap.val()
+            Object.keys(profiles).forEach(profileKey => {
+              if (members.includes(profileKey)){
+                memberNames.push(profiles[profileKey].name)
+              }
+            })
+            bands[bandKey].members = memberNames
+            this.setState({bands})
+          })
+        }
       })
-      this.setState({bands})
     })
   }
 
