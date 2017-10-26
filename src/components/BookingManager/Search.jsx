58f1145e5c51d0b4c527bfb947b82f7f39a2182c
Balázs <info@balazsorban.com>
2017-10-27 01:26:58 +0200

    :tractor: :art: Refactor Search functionality.
    Added extra filters, and sortings. There are some :bug: that has to be
    dealt with. (Read comments.)

--- src/components/BookingManager/Search.jsx
+++ src/components/BookingManager/Search.jsx
@@ -3,7 +3,7 @@ import MenuItem from 'material-ui/MenuItem'
 import TextField from 'material-ui/TextField'
 import {Icon} from '../../utils'
 import SelectField from 'material-ui/SelectField'
-import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar'
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 import Band from '../Band'
 import {Loading, NoResult} from '../../utils'
 
@@ -13,16 +13,17 @@ export default class Search extends Component{
     super(props)
     const {bands, concerts, events, scenes} = props
     this.state = {
-      input: "",
-      genre: "All genres",
-      eventValue: "All events",
-      sceneValue: "All events",
+      query: "",
+      genre: null,
+      event: null,
+      scene: null,
+      sortBy: "",
+      isIncrease: false,
       bands,
       concerts,
       events,
       scenes,
-
-      bandsToOutput : bands && Object.keys(bands),
+      bandsToOutput : bands ? Object.keys(bands) : null
     }
   }
 
@@ -41,41 +42,100 @@ export default class Search extends Component{
   }
 
 
-  //Search for band containing strings in input and update bandsToOutput state
-  searchForBand = () => {
-    let {bands, genre, input} = this.state
-    let bandsToOutput = Object.keys(bands).filter(bandKey => bands[bandKey].name.toLowerCase().includes(input))
-    if (genre !== "All genres"){
-      bandsToOutput = bandsToOutput.filter(bandKey => genre === bands[bandKey].genre)
-    }
-    this.setState({bandsToOutput})
+
+  handleFilters = (key, type) => {
+    this.setState({[type]: key}, () => {
+      const {
+        bands, concerts,
+        query, event, scene, genre
+      } = this.state
+
+      let bandsToOutput = Object.keys(bands)
+      bandsToOutput = bandsToOutput
+      // Filter by Search query
+      .filter(bandKey => bands[bandKey].name.toLowerCase().includes(query))
+      // Filter by genre
+      .filter(bandKey => (genre ? bands[bandKey].genre === genre : bandKey))
+      // Filter by event
+      .filter(bandKey => (event ?
+        bands[bandKey].concerts.some(concertKey => concerts[concertKey] && concerts[concertKey].event !== event) : bandKey
+      ))
+      // Filter by scene
+      .filter(bandKey => (scene ?
+        bands[bandKey].concerts.some(concertKey => concerts[concertKey] && concerts[concertKey].scene !== scene) : bandKey
+      ))
+
+      this.setState({bandsToOutput}, () => this.handleSorters("name", true))
+    })
   }
 
-//Handle input from search
-  handleInputChange = e => {
-    const input = e.target.value.toLowerCase()
-    this.setState({input}, () => this.searchForBand())
-  }
 
-  //onChange genre dropdown list
-  handleGenreChange = (event, index, value) => {
-    this.setState({genre:value}, () => this.searchForBand())
+  handleSorters = (sortBy, isIncrease) => {
+    let {bands, bandsToOutput} = this.state
+    let filtered = bandsToOutput.map(bandKey => [bandKey, bands[bandKey]])
+    filtered.sort((a, b) => (isIncrease ?
+      // FIXME: toString() sorts (as should have been expected) strings, not integers.
+      // For example: 99 > 980
+      a[1][sortBy].toString().localeCompare(b[1][sortBy].toString()) :
+      b[1][sortBy].toString().localeCompare(a[1][sortBy].toString()))
+    )
+    bandsToOutput = filtered.map(band => band[0])
+    this.setState({bandsToOutput, isIncrease})
   }
-  handleEventChange = (event, index, eventValue) => {this.setState({eventValue})}
 
    render(){
-     const {genre, bandsToOutput, bands, concerts, events, scenes, name, eventValue, sceneValue} = this.state
+    const {
+      isIncrease,
+      bands, bandsToOutput,
+      concerts, events, scenes,
+      event, scene, genre,
+      name: reviewerName
+    } = this.state
 
      return(
        <div>
-         <Toolbar className="band-search-toolbar">
-           <ToolbarGroup>
-             <TextField  fullWidth hintText="Search for band" onChange={this.handleInputChange}/>
+         <Toolbar>
+           <div style={{width: "100%",display: "flex", alignItems: "center"}}>
+             <TextField
+               fullWidth
+               hintText="Search for band"
+               onChange={({target:{value}}) => this.handleFilters(value.toLowerCase(), "query")}
+             />
              <Icon name="search" color="grey"/>
-           </ToolbarGroup>
+           </div>
+         </Toolbar>
+         <Toolbar >
            <ToolbarGroup>
-             <SelectField value={genre} onChange={this.handleGenreChange} autoWidth>
-               <MenuItem value={"All genres"} primaryText="All genres" />
+             <ToolbarTitle text="Filter"/>
+             <SelectField
+               value={event}
+               onChange={(event, index, value) => this.handleFilters(value, "event")}
+             >
+               <MenuItem key="All events" value={null} primaryText="All events"/>
+               {Object.keys(events).map(eventKey => (
+                 <MenuItem
+                   key={eventKey}
+                   value={eventKey}
+                   primaryText={events[eventKey].name}
+                 />
+               )
+               )}
+             </SelectField>
+             <SelectField
+               value={scene}
+               onChange={(event, index, value) => this.handleFilters(value, "scene")}
+             >
+               <MenuItem key="All scene" value={null} primaryText="All scene"/>
+               {event && events[event].scenes.map(sceneKey => (
+                 <MenuItem
+                   key={sceneKey}
+                   value={sceneKey}
+                   primaryText={scenes[sceneKey].name}
+                 />
+               ))}
+             </SelectField>
+             <SelectField value={genre} onChange={(event, index, value) => this.handleFilters(value, "genre")} >
+               <MenuItem value={null} primaryText="All genres" />
                <MenuItem value={"Pop"} primaryText="Pop" />
                <MenuItem value={"Rock"} primaryText="Rock" />
                <MenuItem value={"Electric"} primaryText="Electric" />
@@ -83,17 +143,27 @@ export default class Search extends Component{
                <MenuItem value={"RnB"} primaryText="RnB" />
              </SelectField>
            </ToolbarGroup>
-
+         </Toolbar>
+         <Toolbar>
            <ToolbarGroup>
-             <SelectField floatingLabelText="Event" value={eventValue} onChange={this.handleEventChange} autoWidth>
-               <MenuItem value="All events" primaryText="All events" key ={"All events"}/>
-               {Object.keys(events).map(key => {
-                 const {name} = events[key]
-                 return <MenuItem value={name} primaryText={name} key ={key}/>
-               })}
-             </SelectField>
+             <ToolbarTitle text="Sort by"/>
+             <div style={{display: "flex", cursor: "pointer"}} onClick={() => this.handleSorters("name", !isIncrease)}>
+               <p>Name</p>
+               <Icon name="sort_by_alpha"/>
+             </div>
+             <div style={{display: "flex", cursor: "pointer"}} onClick={() => this.handleSorters("genre", !isIncrease)}>
+               <p>Genre</p>
+               <Icon name="sort"/>
+             </div>
+             <div style={{display: "flex", cursor: "pointer"}} onClick={() => this.handleSorters("monthlyListeners", !isIncrease)}>
+               <p>Monthly listeners</p>
+               <Icon name="sort"/>
+             </div>
+             <div style={{display: "flex", cursor: "pointer"}} onClick={() => this.handleSorters("albumSales", !isIncrease)}>
+               <p>Album sales</p>
+               <Icon name="sort"/>
+             </div>
            </ToolbarGroup>
-
          </Toolbar>
          <div className="band-list">
            {bands && concerts ?
